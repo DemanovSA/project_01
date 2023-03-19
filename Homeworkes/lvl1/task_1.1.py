d70309aa2d00a0501b3f45e62b5c22f1de69f4f1
DemanovSA <sergey.demanov@outlook.com>
2023-03-19 17:40:27 +0300

    Added Homeworks folder with tasks

---
+++ Homeworkes/lvl1/task_1.1.py
@@ -0,0 +1,14 @@
+# Задача 1.1.
+
+# Есть строка с перечислением песен
+
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+
+# Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
+# Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+
+# Узнаем какие есть методы у списков
+# from pprint import pprint
+# pprint(dir(my_favorite_songs))
+newplaylist = my_favorite_songs.split(', ')
+print(newplaylist[0], newplaylist[4], newplaylist[1], newplaylist[3])
---
+++ Homeworkes/lvl1/task_1.3.py
@@ -0,0 +1,37 @@
+# Задача 1.3.
+
+# Напишите скрипт, который принимает от пользователя номер месяца, 
+# а возвращает количество дней в нем.
+# Результат проверки вывести на консоль
+# Допущение: в феврале 28 дней
+# Если номер месяца некорректен - сообщить об этом
+
+# Например,
+    # Введите номер месяца: 3
+    # Вы ввели март. 31 дней
+
+    # Введите номер месяца: 2
+    # Вы ввели февраль. 28 дней
+
+    # Введите номер месяца: 15
+    # Такого месяца нет!
+
+import datetime
+import locale
+locale.setlocale(locale.LC_ALL, 'ru_RU.utf8')
+
+numbermonth = int(input('Введите номер месяца:  '))
+if numbermonth < 1 or numbermonth > 12:
+    print('Нет такого месяца')
+else:
+    namemonth = datetime.date(2023, numbermonth, 1).strftime('%B')
+    if int(numbermonth) == 2:
+        days_in_month = 28
+        print(f'Вы ввели {namemonth.lower()}. {days_in_month} дней')
+    elif int(numbermonth) == 4 or int(numbermonth) == 6 or int(numbermonth) == 9 or int(numbermonth) == 11:
+        days_in_month = 30
+        print(f'Вы ввели {namemonth.lower()}. {days_in_month} дней')
+    else:
+        days_in_month = 31
+        print(f'Вы ввели {namemonth.lower()}. {days_in_month} день')
+        
