7da3418b8dbc73ca80e1c32ac99921671e530de9
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-04-04 13:18:58 +0300

    Выполнено

--- Homeworkes/lvl2/task_2.1.py
+++ Homeworkes/lvl2/task_2.1.py
@@ -32,3 +32,29 @@ print(f'{arr}   -> min = {minimum(arr)}, max = {maximum(arr)}')
 print(f'{arr1}  -> min = {minimum(arr1)}, max = {maximum(arr1)}')
 print(f'{arr2}\t\t -> min = {minimum(arr2)}, \t max = {maximum(arr2)}')
 print(f'{arr3}\t\t\t\t -> min = {minimum(arr3)}, \t max = {maximum(arr3)}')
+
+# Хорошо) можно например использовать быструю сортировку
+def quicksort(arr):
+    if len(arr) < 2:
+        # базовый случай, массив с 0 или 1 элементом уже отсортирован
+        return arr
+    else:
+        # рекурсивный случай. выберем опорный элемент pivot
+        pivot = arr[0]
+        # подмассив всех элементов, меньших, чем опорный элемент pivot
+        less = [i for i in arr[1:] if i <= pivot]
+        # подмассив всех элементов, превышающих опорный элемент pivot
+        greater = [i for i in arr[1:] if i > pivot]
+        # обединяем в порядке "сортировка для меньшего подмассива – опорный элемент – сортировка для большего подмассив"
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+# Максимум - это последний элемент отсортированного списка
+def maximum(arr):
+    return quicksort(arr)[-1]
+
+ # Минимум - это первый элемент отсортированного списка
+def minimum(arr):
+    return quicksort(arr)[0]
+print('\nБыстрая сортировка')
+print('min =', minimum([4,6,2,1,9,63,-134,566]))
+print('max =', maximum([4,6,2,1,9,63,-134,566]))
